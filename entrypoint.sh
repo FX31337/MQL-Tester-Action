9ce4c197f0d0a5ba9345c2feecd8f865a4400b5d
kenorb <kenorb@users.noreply.github.com>
2020-05-05 22:23:35 +0100

    Fixes HOME folder permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,21 @@
 # Initialize.
 set -e
 [ -n "$INPUT_OPTVERBOSE" ] && echo "Loading ${BASH_SOURCE[0]}... " >&2
-[ -n "$INPUT_OPTVERBOSE" ] && set
+[ -n "$INPUT_OPTVERBOSE" ] && echo "$USER"
+[ -n "$INPUT_OPTVERBOSE" ] && cat /etc/passwd
+[ -n "$INPUT_OPTVERBOSE" ] && groups
+[ -n "$INPUT_OPTVERBOSE" ] && id && set
+[ -n "$INPUT_OPTVERBOSE" ] && tree -sup /github /home /opt
+
+# Fix HOME folder permissions.
+[ ! -w "$HOME" ] && {
+  sudo chown -vR "$USER" "$HOME"
+}
 
 # Load shell functions.
 # shellcheck disable=SC1091
 source /opt/scripts/.funcs.cmds.inc.sh
 
 # Install platform.
-[ -n "INPUT_OPTTRACE" ] && set -x
+cd "$HOME"
 install_mt "$INPUT_MTVERSION"
